d9c799e4001a6e05c9c83da7fb28e05e6e67f0dc
benjamin <nejibem@gmail.com>
2015-11-27 22:07:30 +1100

    fix deprecated functionality

--- src/AppBundle/Controller/AuthController.php
+++ src/AppBundle/Controller/AuthController.php
@@ -3,7 +3,7 @@
 namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\Security\Core\SecurityContext;
+use Symfony\Component\Security\Core\Security;
 use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
@@ -17,24 +17,24 @@ class AuthController extends Controller
     {
         $session = $request->getSession();
 
-        if( $this->container->get('security.context')->isGranted('IS_AUTHENTICATED_FULLY') )
+        if( $this->container->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_FULLY') )
         {
             return $this->redirect($this->generateUrl('default_security_target'));
         }
 
         // get the login error if there is one
-        if ($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR))
+        if ($request->attributes->has(Security::AUTHENTICATION_ERROR))
         {
-            $error = $request->attributes->get(SecurityContext::AUTHENTICATION_ERROR);
+            $error = $request->attributes->get(Security::AUTHENTICATION_ERROR);
         }
         else
         {
-            $error = $session->get(SecurityContext::AUTHENTICATION_ERROR);
-            $session->remove(SecurityContext::AUTHENTICATION_ERROR);
+            $error = $session->get(Security::AUTHENTICATION_ERROR);
+            $session->remove(Security::AUTHENTICATION_ERROR);
         }
 
         return $this->render('AppBundle:Auth:login.html.twig', [
-            'last_username' => $session->get(SecurityContext::LAST_USERNAME),
+            'last_username' => $session->get(Security::LAST_USERNAME),
             'error'         => $error,
         ]);
     }
